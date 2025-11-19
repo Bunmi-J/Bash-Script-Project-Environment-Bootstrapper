1099a5e58de34e1927c873c4adb1e1f9cc002167
Bunmi-J <obunmi.jegede@gmail.com>
2025-11-19 22:51:36 +0000

    :featured updated  setup.sh

--- setup.sh
+++ setup.sh
@@ -14,18 +14,18 @@ create_virtual_env() {
 virtual_env="/home/obunm/linux_tasks/venv"
 if [ -d $virtual_env ]; then
    echo -e "${BLUE}[info] Virtual environment $virtual_env exists.$NC "
-   echo "$(date): Virtual environment exists" >> $log_file
+   echo -e "${BLUE}$(date): Virtual environment exists.${NC}" >> $log_file
 else
    echo -e "${YELLOW}[WARNING] Virtual environment $virtual_env does not exist, create virtual env.${NC}"
-   echo "$(date): Virtual environment does not exist" >> $log_file
+   echo -e "${YELLOW}$(date): Virtual environment does not exist.${NC}" >> $log_file
    
    python3 -m venv $virtual_env
    if [ $? -eq 0 ]; then
       echo -e "${GREEN}[SUCCESS] Virtual environment :$virtual_env created successfully.${NC}"
-      echo "$(date): virtual environment created successfully" >> $log_file
+      echo -e "${GREEN}$(date): virtual environment created successfully.${NC}" >> $log_file
    else
       echo -e "${RED}[ERROR] Virtual environment creation failed.${NC}"
-      echo "$(date): Virtual environment creation failed" >> $log_file
+      echo -e "${RED}$(date): Virtual environment creation failed. ${NC}" >> $log_file
       exit 1
    fi
 
@@ -37,10 +37,10 @@ echo "Activate the virtual environment"
 source "$virtual_env/bin/activate"
 if [ $virttual_env !=" " ]; then
    echo -e "${GREEN}[SUCCESS] Virtual environment activated successfully.${NC}"
-   echo "$(date): Virtual environment activated successfully" >> $log_file
+   echo -e "${GREEN}$(date): Virtual environment activated successfully.${NC}" >> $log_file
 else
    echo -e "${RED}[ERROR] activation failed.${NC}"
-   echo "$(date): activation failed" >> $log_file
+   echo -e "${RED}$(date): activation failed.${NC}" >> $log_file
 fi
 }
 
@@ -50,11 +50,11 @@ upgrade_pip() {
 python3 -m pip install --upgrade pip
 if [ $? -eq 0 ]; then
    echo -e "${GREEN}[SUCCESS] Pip installation upgrade is successful.${NC}" 
-   echo "$(date): pip installation upgrade is successful" >> $log_file
+   echo -e "${GREEN}$(date): pip installation upgrade is successful.${NC}" >> $log_file
 
 else 
    echo -e "${RED}[ERROR] Pip installation failed.${NC}"
-   echo "$(date): pip installation failed" >> $log_file
+   echo -e "${RED}$(date): pip installation failed.${NC}" >> $log_file
    exit 1
 fi
 }
@@ -65,13 +65,13 @@ gitignore_check() {
 git_ignore="/home/obunm/linux_tasks/venv/.gitignore"
 if [ -f $git_ignore ]; then
    echo -e "${BLUE}[INFO] gitignore $git_ignore file exists. Skip creation.${NC}"
-   echo "$(date): gitignore $git_ignore file exists" >> $log_file 
+   echo -e "${BLUE}$(date): gitignore $git_ignore file exists.${NC}" >> $log_file 
 else
    echo -e "${YELLOW}[WARNING] gitignore $git_ignore file does not exist. create gitignore.${NC}"
    touch $git_ignore
    chmod u+x $git_ignore
    cat  <<EOF > $git_ignore
-   echo "$(date): gitignore $git_ignore file created" >> $log_file
+   echo -e "${BLUE}$(date): gitignore $git_ignore file created.${NC}" >> $log_file
 # Virtual environment
 .venv/
 venv/
@@ -95,21 +95,21 @@ main
 pip install pandas
 if [ $? -eq 0 ]; then
    echo -e "${GREEN}[SUCCESS] Pandas installation is successful.${NC}"
-   echo "$(date): Pandas installed " >> $log_file
+   echo -e "${GREEN}$(date): Pandas installed " >> $log_file
 else 
    echo -e "${RED}[ERROR] Pandas installation failed.${NC}"
-   echo "$(date): Panda installation failed" >> $log_file
+   echo -e "${RED}$(date): Panda installation failed.${NC}" >> $log_file
    exit 1
 fi
 
 # install Python package requests
 pip install requests
 if [ $? -eq 0 ]; then 
-   echo  -e "${GREEN}[SUCCESS] Requests installation is successful.${NC}"
-   echo "$(date): requests installed" >> $log_file
+   echo -e "${GREEN}[SUCCESS] Requests installation is successful.${NC}"
+   echo -e "${GREEN}$(date): requests installed.${NC}" >> $log_file
 else
    echo -e "${RED}[ERROR] requests installation failed.${NC}"
-   echo "$(date): requests installation failed" >> $log_file
+   echo -e "${RED}$(date): requests installation failed.${NC}" >> $log_file
    exit 1
 fi
 
